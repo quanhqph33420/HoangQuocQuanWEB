ba423801abf6e20ea6b779de66f35e20b2296349
quanhqph33420 <quanhqph33420@fpt.edu.vn>
2023-10-02 21:11:57 +0700

    123

--- trangChu.js
+++ trangChu.js
@@ -4,7 +4,16 @@ const btn_pre = document.getElementById("pre")
 const imgs = document.querySelector('.imgs')
 const list_img = document.querySelectorAll(".imgs img")
 const list_dots = document.querySelectorAll('.dots li')
-let img_wi = list_img[0].offsetWidth
+
+const col1_downtiem = document.querySelector(".imgCol1 #downtime")
+const col2_downtiem = document.querySelector(".imgCol2 #downtime")
+const col3_downtiem = document.querySelector(".imgCol3 #downtime")
+const col4_downtiem = document.querySelector(".imgCol4 #downtime")
+
+downtime(col1_downtiem,3 , 19, 0)
+downtime(col2_downtiem,4 , 26, 0)
+downtime(col3_downtiem,1 , 20, 0)
+downtime(col4_downtiem,2 , 10, 0)
 
 list_dots[index].style.color = 'white'
 btn_next.addEventListener("click", () => {
@@ -46,3 +55,27 @@ function changeMenu() {
     });
 }
 changeMenu();
+function downtime(ele, h, m, s) {
+    setInterval(() => {
+        s--;
+        if (s == -1) {
+            s = 60;
+            m--;
+            if (m == 60) {
+                m = 59;
+            }
+
+            if (m == -1) {
+                m = 59;
+                h--;
+                if (h == 24) {
+                    h = 23;
+                }
+                if (h == -1) {
+                    h = 23;
+                }
+            }
+        }
+        ele.innerHTML = `Phim sẽ chiếu sau: ${h}h: ${m}m: ${s}s`
+    }, 1000);
+}
